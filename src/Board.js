7351ab8e10fdb11412a03548378b0c0080c74a96
yeasirahamed <yahamed007@gmail.com>
2021-03-05 23:16:38 +0600

    setup react and update game with board

---
+++ src/Board.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const renderSquare = (i) => <button className="square">{squares[i]}</button>;
+
+  const handleRestart = () => setSquares(Array(9).fill(null));
+  return (
+    <div>
+      <div className="status">status</div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+      <button className="restart" onClick={handleRestart}>
+        Restart
+      </button>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/Game.js
@@ -0,0 +1,11 @@
+import Board from "./Board";
+
+const Game = () => (
+  <div className="game">
+    <div className="game-board">
+      <Board />
+    </div>
+  </div>
+);
+
+export default Game;
